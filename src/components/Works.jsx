22fd36a53d5716a28fabd7b2c100c6931d5de28e
Satoko-chan <49394103+Satoko-chan@users.noreply.github.com>
2020-07-09 16:05:39 +0900

    styled works page using ButtonBase

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,55 +1,164 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
-// import workImg from "./asset/img/workImg.png"
-// import Button from '@material-ui/core/Button';
+import { makeStyles } from "@material-ui/core/styles";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import Typography from "@material-ui/core/Typography";
+
 import YamaLog from "./asset/img/YamaLog.jpg"
 import Peace from "./asset/img/Peace.jpg"
 import Blog from "./asset/img/Blog.jpg"
 import BookChat from "./asset/img/BookChat.png"
 
 
+const images = [
+    {
+        url: YamaLog,
+        title: "YamaLog",
+        comment: "lerhglzelkrh"
+    },
+    {
+        url: Peace,
+        title: "Peace",
+        comment: "lerhglzelkrh"
+    },
+    {
+        url: Blog,
+        title: "Blog",
+        comment: "lerhglzelkrh"
+    },
+    {
+        url: BookChat,
+        title: "BookChat",
+        comment: "lerhglzelkrh"
+    }
+];
 
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: "flex",
+        flexWrap: "wrap",
+        minWidth: 300,
+        width: "100%"
+    },
+    image: {
+        position: "relative",
+        height: 500,
+        width: "50% !important",
+        [theme.breakpoints.down("sm")]: {
+            width: "100% !important", // Overrides inline-style
+            height: 300
+        },
+        "&:hover, &$focusVisible": {
+            zIndex: 1,
+            "& $imageBackdrop": {
+                opacity: 0.5,
+                backgroundColor: theme.palette.common.black
+            },
+            "& $imageMarked": {
+                opacity: 1
+            },
+            "& $imageTitle": {
+                border: "4px solid currentColor"
+            }
+        }
+    },
+    focusVisible: {},
+    imageButton: {
+        position: "absolute",
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: theme.palette.common.white
+    },
+    imageSrc: {
+        position: "absolute",
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        backgroundSize: "cover",
+        backgroundPosition: "center 50%"
+    },
+    imageBackdrop: {
+        position: "absolute",
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        opacity: 0.4,
+        transition: theme.transitions.create("opacity")
+    },
+    imageTitle: {
+        position: "relative",
+        padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) +
+            6}px`
+    },
+    imageMarked: {
+        height: 3,
+        width: 18,
+        backgroundColor: theme.palette.common.white,
+        position: "absolute",
+        bottom: -2,
+        left: "calc(50% - 9px)",
+        transition: theme.transitions.create("opacity")
+    }
+}));
 
 
 function Works() {
+    const classes = useStyles();
     return (
         <div id="works">
             <h2>Works</h2>
-            {/* <Grid container spacing={5} className="workOne" style={{ marginBottom: "10em" }}>
-                <Grid item xs={12} sm={4} className="workImg">
-                    <img src={workImg} width="80%" alt="workImg" />
-                </Grid>
-                <Grid item xs={12} sm={8} className="workText">
-                    <h3 style={{ paddingBottom: "5%" }}>Yama-log</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                    <Button variant="outlined" size="large">GitHub</Button>
-                </Grid>
-            </Grid>
-            <Grid container spacing={5} className="workTwo">
-                <Grid item xs={12} sm={4} className="workImg">
-                    <img src={workImg} width="80%" alt="workImg" />
-                </Grid>
-                <Grid item xs={12} sm={8} className="workText">
-                    <h3 style={{ paddingBottom: "5%" }}>Dokusho-app</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                    <Button variant="outlined" size="large">GitHub</Button>
-                    <Button variant="contained" size="large">Visit Webpage</Button>
-                </Grid>
-            </Grid> */}
 
-            <Grid container spacing={5}>
-                <Grid item xs={12} md={8} className="work1" >
-                    <img src={YamaLog} width="100%" height="400em" alt="YamaLog" />
+            {/* <Grid container spacing={5}>
+                <Grid item xs={12} md={8} >
+                    <div className="work1" >
+                        <img src={YamaLog} width="100%" height="400em" alt="YamaLog" />
+                    </div>
                 </Grid>
                 <Grid item xs={12} md={4} >
                     <img src={Peace} width="100%" height="400em" alt="Peace" className="work2" />
                 </Grid>
                 <Grid item xs={12} md={6} ><img src={Blog} width="100%" height="400em" alt="Blog" className="work3" /></Grid>
                 <Grid item xs={12} md={6} ><img src={BookChat} width="100%" height="400em" alt="BookChat" className="work4" /></Grid>
-            </Grid>
+            </Grid> */}
 
 
-        </div >
+            <div className={classes.root}>
+                {images.map(image => (
+                    <ButtonBase
+                        focusRipple
+                        key={image.title}
+                        className={classes.image}
+                        focusVisibleClassName={classes.focusVisible}
+                    >
+                        <span
+                            className={classes.imageSrc}
+                            style={{
+                                backgroundImage: `url(${image.url})`
+                            }}
+                        />
+                        <span className={classes.imageBackdrop} />
+                        <span className={classes.imageButton}>
+                            <Typography
+                                component="span"
+                                variant="subtitle1"
+                                color="inherit"
+                                className={classes.imageTitle}
+                            >
+                                <h3>{image.title}</h3>
+                                {image.comment}
+                                <span className={classes.imageMarked} />
+                            </Typography>
+                        </span>
+                    </ButtonBase>
+                ))}
+            </div>
+        </div>
     )
 }
 
