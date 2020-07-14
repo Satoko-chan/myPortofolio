90df368268886ae2d5acfd3c2bb7a0af8cbfc1a5
Satoko-chan <49394103+Satoko-chan@users.noreply.github.com>
2020-07-14 14:49:08 +0900

    added StyledP in Footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import styled from "styled-components";
+
+const StyledP = styled.p`
+  color: #717171;
+  font-size: 1.5vh;
+  height: 6vh;
+  line-height: 6vh;
+  @media only screen and (min-width: 1000px) {
+    font-size: 1.8vh;
+    height: 8vh;
+    line-height: 8vh;
+  }
+`;
 
 const year = new Date().getFullYear();
 function Footer() {
     return (
-        <p style={{ height: "8vh", lineHeight: "8vh", fontSize: "2vh" }} className="footer">Created with full of ♡ by Satoko, ©︎{year}</p>
+        <StyledP>Created with full of ♡ by Satoko, ©︎{year}</StyledP>
     );
 }
 
