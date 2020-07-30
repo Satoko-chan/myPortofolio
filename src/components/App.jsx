b40cad202f236321fa2a2d422b152d55d3420061
Satoko-chan <49394103+Satoko-chan@users.noreply.github.com>
2020-07-30 11:49:13 +0900

    added react-router-dom and created subpage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import Heading from "./Heading"
 import About from "./About"
 import Skill from "./Skill"
@@ -6,17 +7,29 @@ import Works from "./Works"
 import Contact from "./Contact"
 import Navbar from "./layout/Navbar"
 import Footer from "./layout/Footer"
+import Portofolio from "./subpage/Portofolio"
+import Yamalog from "./subpage/Yamalog"
+import Api from "./subpage/Api"
+import Blog from "./subpage/Blog"
 
 function App() {
     return (
         <div>
-            <Navbar />
-            <Heading />
-            <About />
-            <Works />
-            <Skill />
-            <Contact />
-            <Footer />
+            <BrowserRouter>
+                <Route exact path="/">
+                    <Navbar />
+                    <Heading />
+                    <About />
+                    <Works />
+                    <Skill />
+                    <Contact />
+                    <Footer />
+                </Route>
+                <Route path="/portofolio" component={Portofolio} />
+                <Route path="/yamalog" component={Yamalog} />
+                <Route path="/api" component={Api} />
+                <Route path="/blog" component={Blog} />
+            </BrowserRouter>
         </div>
     )
 }
--- src/components/subpage/Blog.jsx
+++ src/components/subpage/Blog.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Portofolio() {
+function Blog() {
     return (
         <div>
             <h1>hello</h1>
@@ -8,4 +8,4 @@ function Portofolio() {
     )
 }
 
-export default Portofolio;
+export default Blog;
