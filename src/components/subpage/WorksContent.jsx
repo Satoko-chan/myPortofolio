725d7656a79065dcfc310163cc9dd382f8bac094
Satoko-chan <49394103+Satoko-chan@users.noreply.github.com>
2020-08-19 13:59:50 +0900

    add yamalog url

--- src/components/subpage/WorksContent.jsx
+++ src/components/subpage/WorksContent.jsx
@@ -5,7 +5,7 @@ export const WorksContent = {
         language: ["Ruby on Rails", "SQLite", "Bootstrap", "HTML5", "CSS3"],
         overview: "Are you a mountain lover? Are you searching for an application that you can post your trekking diary and make hiking buddies?  This is an ideal app for you! You can create your own account, post and share some trekking diaries with beautiful picture that you took, 'like' other friends posts. All your personal information are protected and your posted datas will be saved on SQLite database. Let’s give it a try!",
         githubHref: "https://github.com/Satoko-chan/YamaLog",
-        websiteHref: "/"
+        websiteHref: "https://yama-log-123app.herokuapp.com/"
     },
     work2: {
         title: "",
