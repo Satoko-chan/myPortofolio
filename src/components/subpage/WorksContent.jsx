3c3c1fc06e8d691392b7c7202a68d72581598f91
Satoko-chan <49394103+Satoko-chan@users.noreply.github.com>
2020-08-21 12:27:43 +0900

    add language to Yamalog2

--- src/components/subpage/WorksContent.jsx
+++ src/components/subpage/WorksContent.jsx
@@ -2,7 +2,7 @@ export const WorksContent = {
     work1: {
         title: "Yamalog",
         titleText: "A posting tool application using Ruby on Rails",
-        language: ["Ruby on Rails", "JQuery", "SQLite", "Bootstrap", "HTML5", "CSS3"],
+        language: ["Ruby on Rails", "SQLite", "JQuery", "Bootstrap", "HTML5", "CSS3", "Sass"],
         overview: "Are you a mountain lover? Are you searching for an application that you can post your trekking diary and make hiking buddies?  This is an ideal app for you! You can create your own account, post and share some trekking diaries with beautiful picture that you took, 'like' other friends posts. All your personal information are protected and your posted datas will be saved on SQLite database. Let’s give it a try!",
         githubHref: "https://github.com/Satoko-chan/YamaLog",
         websiteHref: "https://yama-log-123app.herokuapp.com/"
